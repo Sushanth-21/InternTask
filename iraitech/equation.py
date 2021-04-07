80f5f2551a5f94b78b7d75633f7173a9342e01e3
sushanth <62661597+Sushanth-21@users.noreply.github.com>
2021-04-07 11:27:41 +0530

    Update equation.py

--- iraitech/equation.py
+++ iraitech/equation.py
@@ -4,6 +4,6 @@ def equation(x,y,n):
     else:
         return (x/y)*equation(x,y,n-1)
 x,y,a,b=map(int,input("Enter x,y,a,b values : ").split())
-# On simplifying the given equation, it is mathematically equal to (x/y) multiplies ab times i.e., power((x/y),a*b)
-n=a*b
+# On simplifying the given equation, it is mathematically equal to (x/y) multiplies a+b times i.e., power((x/y),a+b)
+n=a+b
 print("Result of the equation is {}".format(equation(x,y,n)))
